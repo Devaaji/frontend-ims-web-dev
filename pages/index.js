d5c585245875bab1848c3abaaa20e6a0a69706b9
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-08-15 14:20:32 +0700

    create Home Dashboard

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import { Box } from '@chakra-ui/react';
 import React from 'react';
+import DashboardLayout from '../components/dashboard/DashboardLayout';
 
-export default function Home() {
-  return (
-    <React.Fragment>
-      <Box>Home Dashboard</Box>
-    </React.Fragment>
-  );
-}
+const Home = () => {
+  return <React.Fragment>Home Dashboard</React.Fragment>;
+};
+
+Home.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
+
+export default Home;

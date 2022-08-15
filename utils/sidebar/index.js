c39f9a35484ef64e06771a431c17d6feb939a851
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-08-15 14:18:39 +0700

    Utils Generate in items sidebar

---
+++ utils/sidebar/index.js
@@ -0,0 +1,254 @@
+import { BiBook, BiHome } from 'react-icons/bi';
+import { FiSettings } from 'react-icons/fi';
+
+function generateSidebarItemsAdmin() {
+  return [
+    { name: 'Dashboard', path: '/', icon: BiHome },
+    { name: 'Check Items', path: '/check-items', icon: BiBook },
+    {
+      name: 'Pengaturan',
+      path: '/',
+      icon: FiSettings,
+      sub: [
+        {
+          name: 'Pengaturan Sistem',
+          path: '/system-settings',
+        },
+        {
+          name: 'Pengaturan Website',
+          path: '/frontend-settings',
+        },
+        {
+          name: 'Pengaturan Bahasa',
+          path: '/manage-language',
+        },
+        {
+          name: 'Pengaturan Tema',
+          path: '/theme-settings',
+        },
+        {
+          name: 'Aplikasi Mobile',
+          path: '/mobile-app',
+        },
+      ],
+    },
+    {
+      name: 'Pengaturan',
+      path: '/',
+      icon: FiSettings,
+      sub: [
+        {
+          name: 'Pengaturan Sistem',
+          path: '/system-settings',
+        },
+        {
+          name: 'Pengaturan Website',
+          path: '/frontend-settings',
+        },
+        {
+          name: 'Pengaturan Bahasa',
+          path: '/manage-language',
+        },
+        {
+          name: 'Pengaturan Tema',
+          path: '/theme-settings',
+        },
+        {
+          name: 'Aplikasi Mobile',
+          path: '/mobile-app',
+        },
+      ],
+    },
+    {
+      name: 'Pengaturan',
+      path: '/',
+      icon: FiSettings,
+      sub: [
+        {
+          name: 'Pengaturan Sistem',
+          path: '/system-settings',
+        },
+        {
+          name: 'Pengaturan Website',
+          path: '/frontend-settings',
+        },
+        {
+          name: 'Pengaturan Bahasa',
+          path: '/manage-language',
+        },
+        {
+          name: 'Pengaturan Tema',
+          path: '/theme-settings',
+        },
+        {
+          name: 'Aplikasi Mobile',
+          path: '/mobile-app',
+        },
+      ],
+    },
+    {
+      name: 'Pengaturan',
+      path: '/',
+      icon: FiSettings,
+      sub: [
+        {
+          name: 'Pengaturan Sistem',
+          path: '/system-settings',
+        },
+        {
+          name: 'Pengaturan Website',
+          path: '/frontend-settings',
+        },
+        {
+          name: 'Pengaturan Bahasa',
+          path: '/manage-language',
+        },
+        {
+          name: 'Pengaturan Tema',
+          path: '/theme-settings',
+        },
+        {
+          name: 'Aplikasi Mobile',
+          path: '/mobile-app',
+        },
+      ],
+    },
+    {
+      name: 'Pengaturan',
+      path: '/',
+      icon: FiSettings,
+      sub: [
+        {
+          name: 'Pengaturan Sistem',
+          path: '/system-settings',
+        },
+        {
+          name: 'Pengaturan Website',
+          path: '/frontend-settings',
+        },
+        {
+          name: 'Pengaturan Bahasa',
+          path: '/manage-language',
+        },
+        {
+          name: 'Pengaturan Tema',
+          path: '/theme-settings',
+        },
+        {
+          name: 'Aplikasi Mobile',
+          path: '/mobile-app',
+        },
+      ],
+    },
+    {
+      name: 'Pengaturan',
+      path: '/',
+      icon: FiSettings,
+      sub: [
+        {
+          name: 'Pengaturan Sistem',
+          path: '/system-settings',
+        },
+        {
+          name: 'Pengaturan Website',
+          path: '/frontend-settings',
+        },
+        {
+          name: 'Pengaturan Bahasa',
+          path: '/manage-language',
+        },
+        {
+          name: 'Pengaturan Tema',
+          path: '/theme-settings',
+        },
+        {
+          name: 'Aplikasi Mobile',
+          path: '/mobile-app',
+        },
+      ],
+    },
+    {
+      name: 'Pengaturan',
+      path: '/',
+      icon: FiSettings,
+      sub: [
+        {
+          name: 'Pengaturan Sistem',
+          path: '/system-settings',
+        },
+        {
+          name: 'Pengaturan Website',
+          path: '/frontend-settings',
+        },
+        {
+          name: 'Pengaturan Bahasa',
+          path: '/manage-language',
+        },
+        {
+          name: 'Pengaturan Tema',
+          path: '/theme-settings',
+        },
+        {
+          name: 'Aplikasi Mobile',
+          path: '/mobile-app',
+        },
+      ],
+    },
+    {
+      name: 'Pengaturan',
+      path: '/',
+      icon: FiSettings,
+      sub: [
+        {
+          name: 'Pengaturan Sistem',
+          path: '/system-settings',
+        },
+        {
+          name: 'Pengaturan Website',
+          path: '/frontend-settings',
+        },
+        {
+          name: 'Pengaturan Bahasa',
+          path: '/manage-language',
+        },
+        {
+          name: 'Pengaturan Tema',
+          path: '/theme-settings',
+        },
+        {
+          name: 'Aplikasi Mobile',
+          path: '/mobile-app',
+        },
+      ],
+    },
+    {
+      name: 'Pengaturan',
+      path: '/',
+      icon: FiSettings,
+      sub: [
+        {
+          name: 'Pengaturan Sistem',
+          path: '/system-settings',
+        },
+        {
+          name: 'Pengaturan Website',
+          path: '/frontend-settings',
+        },
+        {
+          name: 'Pengaturan Bahasa',
+          path: '/manage-language',
+        },
+        {
+          name: 'Pengaturan Tema',
+          path: '/theme-settings',
+        },
+        {
+          name: 'Aplikasi Mobile',
+          path: '/mobile-app',
+        },
+      ],
+    },
+  ];
+}
+
+export default generateSidebarItemsAdmin;

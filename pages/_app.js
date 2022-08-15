8c478de74a165484d2d735f9da041ed6e570fc46
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-08-15 08:18:59 +0700

    add in chakra UI function

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react';
+import themes from '../theme';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={themes}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;

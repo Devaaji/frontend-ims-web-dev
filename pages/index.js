f5fa3a50438bed1f96fc905c0eafe11ce2f81d69
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-08-18 10:40:48 +0700

    Add page Dashboard Index

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import Head from 'next/head';
 import DashboardLayout from '../components/dashboard/DashboardLayout';
+import { Box, VStack } from '@chakra-ui/react';
 
 const Home = () => {
-  return <React.Fragment>Home Dashboard</React.Fragment>;
+  return (
+    <React.Fragment>
+      <VStack align="stretch" py="6" px="4" spacing="6">
+        <Head>
+          <title>Dashboard | IMS</title>
+        </Head>
+        <Box>Dashboard Home</Box>
+      </VStack>
+    </React.Fragment>
+  );
 };
 
 Home.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;

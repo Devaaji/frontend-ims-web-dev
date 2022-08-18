9873ad3718893bfa8d3a45098ca427adafdd5018
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-08-18 10:41:50 +0700

    Add NextProggress option

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import themes from '../theme';
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
   return (
     <ChakraProvider theme={themes}>
+      <NextNProgress
+        height={5}
+        color="#D3AF37"
+        options={{ showSpinner: false }}
+      />
       {getLayout(<Component {...pageProps} />)}
     </ChakraProvider>
   );

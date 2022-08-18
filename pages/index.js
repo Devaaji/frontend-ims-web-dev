a8c69837250071d6575964bb062f9472c5b0f003
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-08-18 16:33:31 +0700

    add Chart function

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import Head from 'next/head';
 import DashboardLayout from '../components/dashboard/DashboardLayout';
-import { Box, VStack } from '@chakra-ui/react';
+import {
+  HStack,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  VStack,
+} from '@chakra-ui/react';
+import PageHomeDashboard from '../components/PageDashboard/PageHomeDashboard';
 
 const Home = () => {
   return (
@@ -10,7 +22,40 @@ const Home = () => {
         <Head>
           <title>Dashboard | IMS</title>
         </Head>
-        <Box>Dashboard Home</Box>
+        <HStack borderBottomWidth="1px" pb="6">
+          <Text fontWeight="semibold" color="ims-primary" size="title-medium">
+            Dashboard IMS
+          </Text>
+        </HStack>
+        <PageHomeDashboard />
+        <TableContainer>
+          <Table variant="striped" colorScheme="blue">
+            <Thead>
+              <Tr>
+                <Th>To convert</Th>
+                <Th>into</Th>
+                <Th isNumeric>multiply by</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>inches</Td>
+                <Td>millimetres (mm)</Td>
+                <Td isNumeric>25.4</Td>
+              </Tr>
+              <Tr>
+                <Td>feet</Td>
+                <Td>centimetres (cm)</Td>
+                <Td isNumeric>30.48</Td>
+              </Tr>
+              <Tr>
+                <Td>yards</Td>
+                <Td>metres (m)</Td>
+                <Td isNumeric>0.91444</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
       </VStack>
     </React.Fragment>
   );

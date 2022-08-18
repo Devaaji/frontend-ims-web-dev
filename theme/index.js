5732cb5b656baf00050e58a99a73ce91796ce869
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-08-18 10:40:09 +0700

    add typography in theme

--- theme/index.js
+++ theme/index.js
@@ -3,11 +3,15 @@ import { extendTheme } from '@chakra-ui/react';
 import colors from './components/colors';
 import fonts from './components/fonts';
 import styles from './components/styles';
+import Text from './components/Text';
 
 const theme = extendTheme({
   colors,
   fonts,
   styles,
+  components: {
+    Text,
+  },
 });
 
 export default theme;
---
+++ theme/sharedable/sizes/typography.js
@@ -0,0 +1,15 @@
+const SharedableTypographySizes = {
+  'title-big': {
+    fontSize: ['2xl', null, '3xl', '4xl', '5xl'],
+    fontWeight: 'normal',
+    fontFamily: 'heading',
+    lineHeight: 'shorter',
+  },
+  'title-medium': {
+    fontSize: ['xl', null, '2xl'],
+    fontWeight: 'normal',
+    fontFamily: 'heading',
+  },
+};
+
+export default SharedableTypographySizes;

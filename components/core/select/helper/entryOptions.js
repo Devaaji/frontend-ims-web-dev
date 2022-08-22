13457d1bdc7e9f379712d7f9e9dd75ec978299a3
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-08-22 15:16:39 +0700

    add function EntryOptions

---
+++ components/core/select/helper/entryOptions.js
@@ -0,0 +1,8 @@
+export function generateEntryOptions() {
+  return [
+    { label: '5', value: 5 },
+    { label: '10', value: 10 },
+    { label: '25', value: 25 },
+    { label: '50', value: 50 },
+  ];
+}

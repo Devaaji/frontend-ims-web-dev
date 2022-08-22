42ff3b6c2c33d7633a07f0063d963ebe0ef8e174
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-08-22 15:12:09 +0700

    import button

--- theme/index.js
+++ theme/index.js
@@ -2,6 +2,7 @@ import { extendTheme } from '@chakra-ui/react';
 
 import colors from './components/colors';
 import fonts from './components/fonts';
+import Button from './components/Button';
 import styles from './components/styles';
 import Text from './components/Text';
 
@@ -10,6 +11,7 @@ const theme = extendTheme({
   fonts,
   styles,
   components: {
+    Button,
     Text,
   },
 });

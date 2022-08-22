6617a0b38b32c994743cb8007d9794cb6632fb73
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-08-22 15:17:44 +0700

    Add LineChart Page

---
+++ components/Chart/lineChartDashboard/index.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
+import { Box } from '@chakra-ui/react';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+const LineChartDasboard = () => {
+  return (
+    <>
+      <Box
+        my={{ base: '20px' }}
+        rounded="md"
+        shadow="0px 0px 2px 1px rgba(0, 0, 0, 0.25)"
+        bg="white"
+        p="30px"
+        h="full"
+        w={{ base: 'full', md: '69%' }}
+      >
+        <Line options={options} data={data} />
+      </Box>
+    </>
+  );
+};
+
+export default LineChartDasboard;

0a8aace611df3e95ad122e66ba53d744552f43c8
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-08-24 14:37:49 +0700

    Add Clean code 01 table row and item data

--- components/Modals/ModalDetailItems.js
+++ components/Modals/ModalDetailItems.js
@@ -1,6 +1,9 @@
 import {
+  Box,
   Button,
   ButtonGroup,
+  Flex,
+  HStack,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -10,18 +13,77 @@ import {
   ModalOverlay,
 } from '@chakra-ui/react';
 import React from 'react';
+import { TableToNameItems as TableToName } from '../../utils/tables/TableToNameItems';
 
 const ModalDetailCheckItems = ({ item, isOpen, onClose }) => {
   return (
     <>
-      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        scrollBehavior="inside"
+        isCentered
+      >
         <ModalOverlay bg="blackAlpha.300" />
-        <ModalContent>
+        <ModalContent mx="4" overflow="hidden">
           <ModalHeader>Detail Check Items</ModalHeader>
           <ModalCloseButton />
-          <ModalBody>{item.materialNam}</ModalBody>
+          <ModalBody>
+            <Box w="max" align="stretch">
+              <TableToName tableName="Nomor" item={item.nomor} />
+              <TableToName tableName="Nama Barang" item={item.namaBarang} />
+              <TableToName tableName="Material Nam" item={item.materialNam} />
+              <TableToName tableName="Material Code" item={item.materialCode} />
+              <TableToName tableName="Status" item={item.status} />
+              <TableToName tableName="Category" item={item.kategori} />
+              <TableToName tableName="Min Stock" item={item.minStock} />
+              <TableToName tableName="Max Stock" item={item.maxStock} />
+              <TableToName tableName="In Item" item="12 June 2019" />
+              <TableToName tableName="Out Item" item="25 February 2021" />
+              <TableToName tableName="Nomor" item={item.nomor} />
+              <TableToName tableName="Nama Barang" item={item.namaBarang} />
+              <TableToName tableName="Material Nam" item={item.materialNam} />
+              <TableToName tableName="Material Code" item={item.materialCode} />
+              <TableToName tableName="Status" item={item.status} />
+              <TableToName tableName="Category" item={item.kategori} />
+              <TableToName tableName="Min Stock" item={item.minStock} />
+              <TableToName tableName="Max Stock" item={item.maxStock} />
+              <TableToName tableName="In Item" item="12 June 2019" />
+              <TableToName tableName="Out Item" item="25 February 2021" />
+              <TableToName tableName="Nomor" item={item.nomor} />
+              <TableToName tableName="Nama Barang" item={item.namaBarang} />
+              <TableToName tableName="Material Nam" item={item.materialNam} />
+              <TableToName tableName="Material Code" item={item.materialCode} />
+              <TableToName tableName="Status" item={item.status} />
+              <TableToName tableName="Category" item={item.kategori} />
+              <TableToName tableName="Min Stock" item={item.minStock} />
+              <TableToName tableName="Max Stock" item={item.maxStock} />
+              <TableToName tableName="In Item" item="12 June 2019" />
+              <TableToName tableName="Out Item" item="25 February 2021" />
+              <TableToName tableName="Nomor" item={item.nomor} />
+              <TableToName tableName="Nama Barang" item={item.namaBarang} />
+              <TableToName tableName="Material Nam" item={item.materialNam} />
+              <TableToName tableName="Material Code" item={item.materialCode} />
+              <TableToName tableName="Status" item={item.status} />
+              <TableToName tableName="Category" item={item.kategori} />
+              <TableToName tableName="Min Stock" item={item.minStock} />
+              <TableToName tableName="Max Stock" item={item.maxStock} />
+              <TableToName tableName="In Item" item="12 June 2019" />
+              <TableToName tableName="Out Item" item="25 February 2021" />
+              <TableToName tableName="Nomor" item={item.nomor} />
+              <TableToName tableName="Nama Barang" item={item.namaBarang} />
+              <TableToName tableName="Material Nam" item={item.materialNam} />
+              <TableToName tableName="Material Code" item={item.materialCode} />
+              <TableToName tableName="Status" item={item.status} />
+              <TableToName tableName="Category" item={item.kategori} />
+              <TableToName tableName="Min Stock" item={item.minStock} />
+              <TableToName tableName="Max Stock" item={item.maxStock} />
+              <TableToName tableName="In Item" item="12 June 2019" />
+              <TableToName tableName="Out Item" item="25 February 2021" />
+            </Box>
+          </ModalBody>
 
-          <ModalFooter>
+          <ModalFooter bg="gray.100">
             <ButtonGroup>
               <Button
                 border="1px"

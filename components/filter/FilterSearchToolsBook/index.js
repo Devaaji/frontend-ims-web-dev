84bf3a71051b64fdcf5525e435eccf59238215bb
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-08-24 09:11:43 +0700

    add filter search book in modal

---
+++ components/filter/FilterSearchToolsBook/index.js
@@ -0,0 +1,87 @@
+import {
+  Button,
+  ButtonGroup,
+  HStack,
+  Icon,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Spacer,
+  Tooltip,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { MdOutlineManageSearch } from 'react-icons/md';
+import { FaFileDownload } from 'react-icons/fa';
+
+const FilterSearchToolsBook = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <HStack>
+      <Button
+        rounded="md"
+        leftIcon={<Icon as={MdOutlineManageSearch} fontSize="xl" />}
+        px="8px"
+        onClick={onOpen}
+        border="1px"
+        color="ims-primary"
+        borderColor="ims-primary"
+      >
+        Filter Search
+      </Button>
+      <Spacer />
+      <ButtonGroup>
+        <Button
+          leftIcon={<Icon as={FaFileDownload} color="white" fontSize="md" />}
+          colorScheme="green"
+        >
+          <Tooltip label="Download CSV" fontSize="sm">
+            CSV
+          </Tooltip>
+        </Button>
+        <Button
+          colorScheme="green"
+          leftIcon={<Icon as={FaFileDownload} fontSize="md" />}
+        >
+          <Tooltip label="Download XLSX" fontSize="sm">
+            XLSX
+          </Tooltip>
+        </Button>
+      </ButtonGroup>
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent mx={{ base: '2', md: 'none' }}>
+          <ModalHeader>FIlter Search Tools Book</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>FIlter Search</ModalBody>
+          <ModalFooter>
+            <ButtonGroup>
+              <Button
+                border="1px"
+                color="ims-primary"
+                borderColor="ims-primary"
+                aria-label="close modal"
+                onClick={onClose}
+              >
+                Batal
+              </Button>
+              <Button
+                type="submit"
+                bg="ims-primary"
+                color="white"
+                _hover={{ bg: 'button-hover' }}
+              >
+                Cari
+              </Button>
+            </ButtonGroup>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </HStack>
+  );
+};
+
+export default FilterSearchToolsBook;

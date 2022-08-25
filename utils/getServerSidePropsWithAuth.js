279f2c7ca8e3e7605e15818499c7840b1c526ec7
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-08-25 15:19:21 +0700

    create function serversideprops auth

--- utils/getServerSidePropsWithAuth.js
+++ utils/getServerSidePropsWithAuth.js
@@ -1,1 +1,37 @@
-export const getServerSidePropsWithAuth = async () => {};
+const dataNotion = null;
+
+export const getServerSidePropsWithNoAuth = async () => {
+  const email = dataNotion;
+  const accessToken = dataNotion;
+
+  if (email && accessToken) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
+
+export const getServerSidePropsWithAuth = async () => {
+  const email = dataNotion;
+  const accessToken = dataNotion;
+
+  if (!email && !accessToken) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};

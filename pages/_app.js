7c47bfbfe7e39d2180131abfdf3275dd35ee7145
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-08-25 15:20:11 +0700

    add react-query provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
+import React, { useState } from 'react';
+
 import { ChakraProvider } from '@chakra-ui/react';
 import themes from '../theme';
 import NextNProgress from 'nextjs-progressbar';
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
+
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <ChakraProvider theme={themes}>
       <NextNProgress
@@ -11,7 +21,11 @@ function MyApp({ Component, pageProps }) {
         color="#D3AF37"
         options={{ showSpinner: false }}
       />
-      {getLayout(<Component {...pageProps} />)}
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          {getLayout(<Component {...pageProps} />)}
+        </Hydrate>
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }

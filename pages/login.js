2412ae12c4bd950d6c5596a449ea0b1cd485a7c8
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-09-01 16:12:43 +0700

    get data in api login and change UI

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { NextSeo } from 'next-seo';
 import NextLink from 'next/link';
+import { useRouter } from 'next/router';
 
 import {
+  Alert,
+  AlertIcon,
   Box,
   Button,
   Center,
@@ -11,7 +14,6 @@ import {
   FormControl,
   FormErrorMessage,
   FormLabel,
-  Heading,
   IconButton,
   Input,
   InputGroup,
@@ -19,6 +21,7 @@ import {
   Link,
   Stack,
   Text,
+  useBoolean,
   useDisclosure,
 } from '@chakra-ui/react';
 
@@ -28,68 +31,157 @@ import { useForm } from 'react-hook-form';
 import { BsFillEyeFill, BsFillEyeSlashFill } from 'react-icons/bs';
 import { loginSchema } from '../utils/schema/AuthenticationSchema';
 import PindadLogo from '../components/core/pindadlogo';
+import useAxios from '../components/hooks/useAxios';
+import useAuthUserStore from '../store/useAuthUserStore';
+import useToastNetworkError from '../components/hooks/useToastNetworkError';
+import PindadLogoWhite from '../components/core/pindadlogowhite';
 
 const LoginPage = () => {
+  const router = useRouter();
+  const [errors, setErrors] = useState();
+  const [isLoading, setIsloading] = useBoolean();
+  const showToastNetworkError = useToastNetworkError();
+
   const { isOpen: isPasswordOpen, onToggle: onPasswordToggle } =
     useDisclosure();
 
-  const { register, formState, handleSubmit } = useForm({
+  const { register, formState, handleSubmit, setError } = useForm({
     resolver: yupResolver(loginSchema),
   });
 
-  const onSubmit = (data) => console.log(data);
+  const [, createToken] = useAxios(
+    {
+      url: '/create-token',
+      method: 'POST',
+    },
+    { manual: true }
+  );
+  const setCreateToken = useAuthUserStore((state) => state.setCreateToken);
+
+  const updateData = async () => {
+    const create = await createToken({
+      data: {
+        name: 'ims-services',
+        device_id: '9b2767f0-d441-4e8d-83fc-e44640311ed0',
+        device_type: 'e726d357-b35c-4378-8bbd-f86e0661edc9',
+        secret_key: '1m5-servic3',
+      },
+    });
+    setCreateToken(create.data.data.token);
+  };
+
+  const [, makeLogin] = useAxios(
+    { url: '/auth/login', method: 'POST' },
+    { manual: true }
+  );
+
+  const onSubmit = async (data) => {
+    setIsloading.on();
+    try {
+      updateData();
+      await makeLogin({ data });
+      router.push('/');
+    } catch (error) {
+      setIsloading.off();
+      if (error) {
+        const errorMessage = error.response.data.message;
+        setErrors(errorMessage);
+      } else showToastNetworkError();
+    }
+  };
 
   return (
     <React.Fragment>
       <NextSeo title="Login" titleTemplate="%s | IMS" />
-      <Flex
-        flexDirection="column"
-        w="100wh"
-        h="100vh"
-        justifyContent="center"
-        align="center"
-      >
-        <Stack
+      <Flex flexDirection="row" w="100wh" h="100vh">
+        <Box
+          display={{ base: 'none', md: 'none', xl: 'block' }}
+          h="full"
+          py="20px"
+          px="60px"
+          w="50%"
+          bgGradient="linear(to-br, #010080 50%, #D3AF37)"
+          boxShadow={'0px 0px 6px 1px rgba(0, 0, 0, 0.25);'}
+        >
+          <Flex
+            position="relative"
+            direction="column"
+            justify="center"
+            h="full"
+          >
+            <Box position="absolute" top={0} left={0}>
+              <PindadLogoWhite multiplySize={1} />
+            </Box>
+            <Text fontWeight="bold" color="white" fontSize="6xl">
+              Inventory
+            </Text>
+            <Text fontWeight="bold" color="white" fontSize="6xl">
+              Management
+            </Text>
+            <Text fontWeight="bold" color="white" fontSize="6xl">
+              System
+            </Text>
+          </Flex>
+        </Box>
+        <Flex
           as="form"
           onSubmit={handleSubmit(onSubmit)}
-          flexDir="column"
-          mb="4"
-          justifyContent="center"
+          w={{ base: 'full', md: 'full', xl: '50%' }}
+          h="full"
+          position="relative"
+          bg="white"
+          direction="column"
+          justify="center"
           alignItems="center"
         >
-          <Heading w="370px" textAlign="center" color="ims-primary">
-            Silahkan Masuk Akun IMS Kamu!
-          </Heading>
+          <Flex
+            display={{ xl: 'none' }}
+            position="absolute"
+            top={0}
+            py="5"
+            justify="center"
+            w="full"
+          >
+            <Center>
+              <PindadLogo multiplySize={0.8} />
+            </Center>
+          </Flex>
           <Box
-            w={{ base: 'full', md: '400px' }}
-            h="max"
+            w="full"
             bg="white"
-            p="34px"
-            boxShadow={'0px 0px 6px 1px rgba(0, 0, 0, 0.25);'}
-            borderRadius="15px"
+            p={{ base: '25px', md: '120px', xl: '157px' }}
+            rounded="sm"
           >
             <Stack spacing="6">
-              <Center>
-                <PindadLogo multiplySize={1.5} />
-              </Center>
               <Box>
-                <Text fontSize="2xl" fontWeight="semibold">
+                <Text
+                  bgGradient="linear(to-l, #D3AF37, #010080)"
+                  bgClip="text"
+                  fontSize={{ base: '2xl', md: '4xl' }}
+                  fontWeight="bold"
+                >
                   Login
                 </Text>
               </Box>
+              {errors && (
+                <Alert status="error" variant="left-accent">
+                  <AlertIcon />
+                  {errors}
+                </Alert>
+              )}
               <FormControl
-                id="email"
-                isInvalid={!!formState.errors?.email}
-                errortext={formState.errors?.email?.message}
+                id="username"
+                isInvalid={!!formState.errors?.username}
+                errortext={formState.errors?.username?.message}
               >
-                <FormLabel>Email</FormLabel>
+                <FormLabel>Username</FormLabel>
                 <Input
-                  type="email"
-                  placeholder="Mohon masukkan Email Anda"
-                  {...register('email')}
+                  type="text"
+                  placeholder="Mohon masukkan Username Anda"
+                  {...register('username')}
                 />
                 <FormErrorMessage fontSize="xs">
-                  {formState.errors?.email?.message}
+                  {formState.errors?.username?.message}
                 </FormErrorMessage>
               </FormControl>
               <FormControl
@@ -141,14 +233,20 @@ const LoginPage = () => {
                 type="submit"
                 bg="ims-primary"
                 color="white"
-                _hover={{ bg: 'button-hover' }}
+                transitionProperty="common"
+                transitionDuration="normal"
+                _hover={{
+                  bgGradient: 'linear(to-l, #D3AF37 5%, #010080 50%)',
+                  transform: 'scale(1.01)',
+                }}
                 fontWeight="bold"
+                isLoading={isLoading}
               >
                 Masuk
               </Button>
             </Stack>
           </Box>
-        </Stack>
+        </Flex>
       </Flex>
     </React.Fragment>
   );

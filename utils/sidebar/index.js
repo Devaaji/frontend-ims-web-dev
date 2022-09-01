e8c00350171b08b3e3d0dfeeed07387161899f9d
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-09-01 16:10:34 +0700

    add component in sidebar

--- utils/sidebar/index.js
+++ utils/sidebar/index.js
@@ -1,10 +1,54 @@
-import { BiBook, BiHome } from 'react-icons/bi';
+import { BiBook, BiHome, BiPurchaseTagAlt } from 'react-icons/bi';
 import { FaToolbox } from 'react-icons/fa';
-import { FiSettings } from 'react-icons/fi';
+import { FiBox, FiSettings } from 'react-icons/fi';
+import { MdTableView } from 'react-icons/md';
 
 function generateSidebarItemsAdmin() {
   return [
     { name: 'Dashboard', path: '/', icon: BiHome },
+    { name: 'Master Material', path: '/master-material', icon: MdTableView },
+    {
+      name: 'Material Stock',
+      path: '/',
+      icon: FiBox,
+      sub: [
+        {
+          name: 'Transaction Material Stock',
+          path: '/menu-settings',
+        },
+        {
+          name: 'Master Material Batch',
+          path: '/menu-settings',
+        },
+        {
+          name: 'Master Vendor',
+          path: '/menu-settings',
+        },
+      ],
+    },
+    {
+      name: 'Purchase Order',
+      path: '/',
+      icon: BiPurchaseTagAlt,
+      sub: [
+        {
+          name: 'PO Header',
+          path: '/menu-settings',
+        },
+        {
+          name: 'PO Item',
+          path: '/menu-settings',
+        },
+        {
+          name: 'PO Component',
+          path: '/menu-settings',
+        },
+        {
+          name: 'PO Assignment',
+          path: '/menu-settings',
+        },
+      ],
+    },
     { name: 'Check Items', path: '/check-items', icon: BiBook },
     { name: 'Tools Book', path: '/tools-book', icon: FaToolbox },
     {

ef8726c164c31459689aaf72fabdcf99b540a205
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-09-02 14:56:58 +0700

    add tes list master material

--- pages/master-material.js
+++ pages/master-material.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import { Button } from '@chakra-ui/react';
+import React, { useEffect } from 'react';
 import DashboardLayout from '../components/dashboard/DashboardLayout';
+import useAxios from '../components/hooks/useAxios';
 
 const MasterMaterial = () => {
+  const [isLoading, listMasterMaterial] = useAxios(
+    {
+      url: '/material/list',
+      method: 'POST',
+    },
+    { manual: true }
+  );
+
+  const ListMasterMaterial = async () => {
+    const getData = await listMasterMaterial({
+      data: {
+        limit: 30,
+        page: 1,
+        order: 'master_material_code',
+        sort: 'ASC',
+      },
+    });
+    console.log('List Material', getData);
+  };
+
+  useEffect(() => {
+    ListMasterMaterial();
+  }, []);
+
   return (
     <>
+      <Button onClick={ListMasterMaterial}>POST</Button>
       <div>Material Master</div>
     </>
   );

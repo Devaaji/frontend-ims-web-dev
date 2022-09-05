44b194f4db5481ba9635699f11010eed5d08afc4
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-09-05 14:39:28 +0700

    change font nunito

--- theme/components/fonts.js
+++ theme/components/fonts.js
@@ -1,6 +1,6 @@
 const fonts = {
   heading: `Open Sans, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
-  body: `Open Sans, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
+  body: `Nunito, sans-serif`,
   mono: `ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;`,
   button:
     'Poppins, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
--- theme/components/styles.js
+++ theme/components/styles.js
@@ -1,8 +1,7 @@
 const styles = {
   global: {
     'html, body': {
-      fontFamily:
-        'Open Sans, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+      fontFamily: 'Nunito, sans-serif',
       backgroundColor: '#F1F5F9',
     },
     a: {
